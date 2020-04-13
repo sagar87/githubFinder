46a95ac7008c10dd353bb7656853756334ca69e6
Harald Sagar Vöhringer <Sagar@b1161-c542.fritz.box>
2020-04-13 17:15:29 +0100

    Converted Search and User component to use hooks.

--- src/components/users/Search.jsx
+++ src/components/users/Search.jsx
@@ -1,51 +1,48 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-class Search extends Component {
-  state = {
-    query: "",
-  };
-  handleChange = (e) => {
+const Search = ({ showClear, clearSearch, searchCallback, setAlert }) => {
+  const [query, setQuery] = useState("");
+  const handleChange = (e) => {
     const { name, value } = e.target;
-    this.setState({ [name]: value });
+    setQuery(value);
   };
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    if (this.state.query === "") {
-      this.props.setAlert("Please enter something", "light");
+    if (query === "") {
+      setAlert("Please enter something", "light");
     } else {
-      this.props.searchCallback(this.state.query);
-      this.setState({ query: "" });
+      searchCallback(query);
+      setQuery("");
     }
   };
-  static propTypes = {
-    searchCallback: PropTypes.func.isRequired,
-    clearSearch: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired,
-    setAlert: PropTypes.func.isRequired,
-  };
-  render() {
-    const { showClear, clearSearch } = this.props;
-    return (
-      <React.Fragment>
-        <form onSubmit={this.handleSubmit} className="form">
-          <input
-            value={this.state.query}
-            onChange={this.handleChange}
-            type="text"
-            name="query"
-            placeholder="Search Users ..."
-          />
-          <input type="submit" className="btn btn-dark btn-block" />
-        </form>
-        {showClear && (
-          <button className="btn btn-light btn-block" onClick={clearSearch}>
-            Clear
-          </button>
-        )}
-      </React.Fragment>
-    );
-  }
-}
+
+  return (
+    <React.Fragment>
+      <form onSubmit={handleSubmit} className="form">
+        <input
+          value={query}
+          onChange={handleChange}
+          type="text"
+          name="query"
+          placeholder="Search Users ..."
+        />
+        <input type="submit" className="btn btn-dark btn-block" />
+      </form>
+      {showClear && (
+        <button className="btn btn-light btn-block" onClick={clearSearch}>
+          Clear
+        </button>
+      )}
+    </React.Fragment>
+  );
+};
+
+Search.propTypes = {
+  searchCallback: PropTypes.func.isRequired,
+  clearSearch: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired,
+  setAlert: PropTypes.func.isRequired,
+};
 
 export default Search;
--- src/components/users/User.jsx
+++ src/components/users/User.jsx
@@ -1,104 +1,103 @@
-import React, { Component, Fragment } from "react";
+import React, { useEffect, Fragment } from "react";
 import { Spinner } from "../layout/Spinner";
 import { PropTypes } from "prop-types";
 import { Link } from "react-router-dom";
 import Repos from "../repos/Repos";
 
-class User extends Component {
-  componentDidMount() {
-    console.log("here");
-    this.props.getUser(this.props.match.params.login);
-    this.props.getUserRepos(this.props.match.params.login);
-  }
-  static propTypes = {
-    loading: PropTypes.bool,
-    user: PropTypes.object.isRequired,
-    getUser: PropTypes.func.isRequired,
-    getUserRepos: PropTypes.func.isRequired,
-    repos: PropTypes.array.isRequired,
-  };
-  render() {
-    const {
-      name,
-      avatar_url,
-      location,
-      bio,
-      blog,
-      login,
-      html_url,
-      followers,
-      following,
-      public_repos,
-      public_gists,
-      company,
-      hireable,
-    } = this.props.user;
-    const { loading } = this.props;
+const User = ({ user, repos, loading, getUser, getUserRepos, match }) => {
+  useEffect(() => {
+    getUser(match.params.login);
+    getUserRepos(match.params.login);
+    // eslint-disable-next-line
+  }, []);
 
-    if (loading) {
-      return <Spinner />;
-    }
+  const {
+    name,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    company,
+    hireable,
+  } = user;
 
-    return (
-      <Fragment>
-        <Link to="/" className="btn btn-light">
-          Back to search
-        </Link>
-        Hireable :{" "}
-        {hireable ? (
-          <i className="fas fa-check text-success"></i>
-        ) : (
-          <i className="fas fa-times-circle text-danger"></i>
-        )}
-        <div className="card grid-2">
-          <div className="all-center">
-            <img
-              src={avatar_url}
-              alt=""
-              className="round-img"
-              style={{ width: "150px" }}
-            />
-          </div>
-          <h1>{name}</h1>
-          <p>Location {location}</p>
-          <div className="">
-            {bio && (
-              <Fragment>
-                <h3>Bio</h3>
-                <p>{bio}</p>
-              </Fragment>
-            )}
-            <a href={html_url} className="btn btn-dark my-1">
-              Visit Github
-            </a>
-            <ul>
-              <li>
-                {login && (
-                  <Fragment>
-                    <strong>Username: </strong> {login}
-                  </Fragment>
-                )}
-              </li>
-              <li>
-                {login && (
-                  <Fragment>
-                    <strong>Company: </strong> {company}
-                  </Fragment>
-                )}
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="card text-center">
-          <div className="badge badge-primary">Followers: {followers}</div>
-          <div className="badge badge-success">Following: {following}</div>
-          <div className="badge badge-light">Public repos: {public_repos}</div>
-          <div className="badge badge-dark">Public gists: {public_gists}</div>
-        </div>
-        <Repos repos={this.props.repos} />
-      </Fragment>
-    );
+  if (loading) {
+    return <Spinner />;
   }
-}
+
+  return (
+    <Fragment>
+      <Link to="/" className="btn btn-light">
+        Back to search
+      </Link>
+      Hireable :{" "}
+      {hireable ? (
+        <i className="fas fa-check text-success"></i>
+      ) : (
+        <i className="fas fa-times-circle text-danger"></i>
+      )}
+      <div className="card grid-2">
+        <div className="all-center">
+          <img
+            src={avatar_url}
+            alt=""
+            className="round-img"
+            style={{ width: "150px" }}
+          />
+        </div>
+        <h1>{name}</h1>
+        <p>Location {location}</p>
+        <div className="">
+          {bio && (
+            <Fragment>
+              <h3>Bio</h3>
+              <p>{bio}</p>
+            </Fragment>
+          )}
+          <a href={html_url} className="btn btn-dark my-1">
+            Visit Github
+          </a>
+          <ul>
+            <li>
+              {login && (
+                <Fragment>
+                  <strong>Username: </strong> {login}
+                </Fragment>
+              )}
+            </li>
+            <li>
+              {login && (
+                <Fragment>
+                  <strong>Company: </strong> {company}
+                </Fragment>
+              )}
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="card text-center">
+        <div className="badge badge-primary">Followers: {followers}</div>
+        <div className="badge badge-success">Following: {following}</div>
+        <div className="badge badge-light">Public repos: {public_repos}</div>
+        <div className="badge badge-dark">Public gists: {public_gists}</div>
+      </div>
+      <Repos repos={repos} />
+    </Fragment>
+  );
+};
+
+User.propTypes = {
+  loading: PropTypes.bool,
+  user: PropTypes.object.isRequired,
+  getUser: PropTypes.func.isRequired,
+  getUserRepos: PropTypes.func.isRequired,
+  repos: PropTypes.array.isRequired,
+};
 
 export default User;
